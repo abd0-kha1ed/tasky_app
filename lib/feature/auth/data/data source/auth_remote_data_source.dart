a3d0b62aaed080397aa3cad73dea676b74f0a2c9
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-03-15 23:32:59 +0200

    add auth rope imple

--- lib/feature/auth/data/data source/auth_remote_data_source.dart
+++ lib/feature/auth/data/data source/auth_remote_data_source.dart
@@ -1,6 +1,8 @@
+import 'package:dartz/dartz.dart';
 import 'package:to_do_app/core/api/api_consumer.dart';
 import 'package:to_do_app/core/api/end_points.dart';
-
+import 'package:to_do_app/core/errors/error_model.dart';
+import 'package:to_do_app/core/errors/excptions.dart';
 import 'package:to_do_app/feature/auth/data/models/login_model.dart';
 import 'package:to_do_app/feature/auth/data/models/register_model.dart';
 
@@ -9,22 +11,26 @@ class AuthRemoteDataSource {
 
   AuthRemoteDataSource({required this.api});
 
-  Future<LoginModel> login({
+  Future<Either<ServerException, LoginModel>> login({
     required num phone,
     required String password,
   }) async {
-    final response = await api.post(
-      EndPoint.signIn,
-      data: {
-        ApiKey.phoneNumber: phone,
-        ApiKey.password: password,
-      },
-    );
+    try {
+      final response = await api.post(
+        EndPoint.signIn,
+        data: {
+          ApiKey.phoneNumber: phone,
+          ApiKey.password: password,
+        },
+      );
 
-    return LoginModel.fromJson(response);
+      return Right(LoginModel.fromJson(response));
+    } on ServerException catch (e) {
+      return Left(ServerException(errModel: e.errModel));
+    }
   }
 
-  Future<RegisterModel> register({
+  Future<Either<ServerException, RegisterModel>> register({
     required String name,
     required num phoneNumber,
     required num experienceYear,
@@ -32,18 +38,22 @@ class AuthRemoteDataSource {
     required String address,
     required String password,
   }) async {
-    final response = await api.post(
-      EndPoint.signUp,
-      data: {
-        ApiKey.name: name,
-        ApiKey.phoneNumber: phoneNumber,
-        ApiKey.experinceYear: experienceYear,
-        ApiKey.expericneLevel: experienceLevel,
-        ApiKey.address: address,
-        ApiKey.password: password
-      },
-    );
+    try {
+      final response = await api.post(
+        EndPoint.signUp,
+        data: {
+          ApiKey.name: name,
+          ApiKey.phoneNumber: phoneNumber,
+          ApiKey.experinceYear: experienceYear,
+          ApiKey.expericneLevel: experienceLevel,
+          ApiKey.address: address,
+          ApiKey.password: password,
+        },
+      );
 
-    return RegisterModel.fromJson(response);
+      return Right(RegisterModel.fromJson(response));
+    } on ServerException catch (e) {
+      return Left(ServerException(errModel: e.errModel));
+    }
   }
 }
---
+++ lib/feature/auth/data/repos/auth_rope_impl.dart
@@ -0,0 +1,36 @@
+import 'package:dartz/dartz.dart';
+import 'package:to_do_app/core/errors/excptions.dart';
+import 'package:to_do_app/feature/auth/data/data%20source/auth_remote_data_source.dart';
+import 'package:to_do_app/feature/auth/domain/entites/login_entites.dart';
+import 'package:to_do_app/feature/auth/domain/entites/register_entites.dart';
+import 'package:to_do_app/feature/auth/domain/repos/auth_repo.dart';
+
+class AuthRopeImpl implements AuthRepo {
+  final AuthRemoteDataSource authRemoteDataSource;
+
+  AuthRopeImpl({required this.authRemoteDataSource});
+
+  @override
+  Future<Either<ServerException, LoginEntites>> login(
+      {required num phoneNumber, required String password}) async {
+    return await authRemoteDataSource.login(
+        phone: phoneNumber, password: password);
+  }
+
+  @override
+  Future<Either<ServerException, RegisterEntites>> register(
+      {required String name,
+      required num phoneNumber,
+      required num experienceYear,
+      required String experienceLevel,
+      required String address,
+      required String password}) async {
+    return await authRemoteDataSource.register(
+        name: name,
+        phoneNumber: phoneNumber,
+        experienceYear: experienceYear,
+        experienceLevel: experienceLevel,
+        address: address,
+        password: password);
+  }
+}
