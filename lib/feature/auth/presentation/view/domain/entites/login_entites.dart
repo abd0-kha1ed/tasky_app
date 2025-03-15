6c630a3974f73ad8b85da00106c0b2095d14f331
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-03-15 16:18:09 +0200

    add login entites

---
+++ lib/feature/auth/presentation/view/domain/entites/login_entites.dart
@@ -0,0 +1,8 @@
+class LoginEntites {
+  final String id;
+  final String phoneNumber;
+  final String password;
+
+  LoginEntites(
+      {required this.id, required this.phoneNumber, required this.password});
+}
