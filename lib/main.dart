9da4327537a1e0adedd8cf23f5f96f2e848f91bf
abdelrahman <bad606609@gmail.com>
2025-03-15 15:16:51 +0200

    add device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:responsive_framework/responsive_wrapper.dart';
 import 'package:to_do_app/core/utils/app_routers.dart';
 
 void main() {
-  runApp(const TaskyApp());
+  runApp(DevicePreview(
+    builder: (context) {
+      return const TaskyApp();
+    }
+  ));
 }
 
 class TaskyApp extends StatelessWidget {
@@ -11,18 +15,20 @@ class TaskyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       routerConfig: AppRouters.router,
-       builder: (context, child) => ResponsiveWrapper.builder(child,
-          maxWidth: 1200,
-          minWidth: 480,
-          defaultScale: true,
-          breakpoints: [
-            const ResponsiveBreakpoint.resize(480, name: MOBILE),
-            const ResponsiveBreakpoint.autoScale(800, name: TABLET),
-            const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-          ],
-          background: Container(color: const Color(0xFFF5F5F5))),
+      //  builder: (context, child) => ResponsiveWrapper.builder(child,
+      //     maxWidth: 1200,
+      //     minWidth: 480,
+      //     defaultScale: true,
+      //     breakpoints: [
+      //       const ResponsiveBreakpoint.resize(480, name: MOBILE),
+      //       const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+      //       const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+      //     ],
+      //     background: Container(color: const Color(0xFFF5F5F5))),
     );
   }
 }
