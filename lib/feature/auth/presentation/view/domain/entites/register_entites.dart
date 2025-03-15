e49685122d2d112213470bc89efaf0dc3cfaf8b8
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-03-15 16:20:59 +0200

    register_entites

---
+++ lib/feature/auth/presentation/view/domain/entites/register_entites.dart
@@ -0,0 +1,16 @@
+class RegisterEntites {
+  final String name;
+  final String phoneNumber;
+  final num experienceYear;
+  final String experienceLevel;
+  final String address;
+  final String password;
+
+  RegisterEntites(
+      {required this.name,
+      required this.phoneNumber,
+      required this.experienceYear,
+      required this.experienceLevel,
+      required this.address,
+      required this.password});
+}
